9a070b7951d68aea8e3f0f333c99a59fc0092f19
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-07-13 20:10:16 +0300

    update package.json, pnpm-lock.yaml, app.controller.ts and main.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,25 @@
-import { Controller, Get, Body } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Parser } from '@json2csv/plainjs';
 import { flatten, unwind } from '@json2csv/transforms';
+import { ApiTags, ApiOperation, ApiBody, ApiHideProperty } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('CSV Converter')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Post('json-to-csv')
+  @ApiOperation({ summary: 'Convert JSON to CSV' })
+  @ApiBody({ type: Object, description: 'JSON data to convert' })
   convertJsonToCsv(@Body() body) {
     return this.appService.jsonToCsv(body);
   }
 
-  @Get('lol-match-data')
+  @Post('lol-match-data')
+  @ApiOperation({ summary: 'Convert LoL Match Data JSON to CSV' })
+  @ApiBody({ type: Object, description: 'LoL Match Data JSON to convert' })
+  @ApiHideProperty()
   convertLoLMatchDataJsonToCsv(@Body() body) {
     return this.appService.lolMatchDataJsonToCsv(body.info.participants);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('JSON to CSV')
+    .setDescription('JSON to CSV API, mainly for League of Legends match data.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb' }));
   await app.listen(3000);
